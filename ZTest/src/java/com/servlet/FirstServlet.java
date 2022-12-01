de4cc7e62f557ab8613057d50637877260b39458
sandesh.gowda <sandeshgowda2021@gmail.com>
2022-12-01 12:08:34 +0530

    Out put in Console by PrintWriter in Service Method

--- ZTest/src/java/com/servlet/FirstServlet.java
+++ ZTest/src/java/com/servlet/FirstServlet.java
@@ -1,5 +1,7 @@
 package com.servlet;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -7,8 +9,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public class FirstServlet implements Servlet{
-    //Life Cycle Methods
     
+    //Life Cycle Methods
     ServletConfig conf;
     
     public void init(ServletConfig config){
@@ -19,6 +21,13 @@ public class FirstServlet implements Servlet{
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         System.out.println("Serviceing.........");
+        
+        //Set the Content type of the Response
+        res.setContentType("text/html");
+        PrintWriter out =res.getWriter();
+        out.println("<h1>This is My Output From Servlet Method:</h1>");
+        out.println("<h1>Todays date and time is" + new Date().toString()+"</h1>");
+
     
     }
 
@@ -38,13 +47,4 @@ public class FirstServlet implements Servlet{
     public String getServletInfo() {
        return "This Servlet is Created By Sandesh Gowda";
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
